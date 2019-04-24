55f64918a7e537f5bc0f14c10ace7428fdc5e885
Camilo Mason <camilomason@Camilos-MacBook-Pro.local>
2019-04-23 22:11:26 -0400

    fixing temperature conversion

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -3,18 +3,17 @@ class ConverterController < ApplicationController
     @input = params[:input].to_f
     @first = params[:first]
     @second = params[:second]
-    @output = 0.to_f
 
     if @first === "f" && @second === "c"
-      @output = (@input-32)*(5/9)
+      @output = (@input - 32) * 5/9
     elsif @first === "f" && @second === "k"
-      @output = (@input-32)*(5/9)+273.15
+      @output = (@input - 32) * 5/9 + 273.15
     elsif @first === "c" && @second === "f"
-      @output = (@input*(9/5))+32
+      @output = @input * 9/5 + 32.0
     elsif @first === "c" && @second === "k"
       @output = @input + 273.15
     elsif @first === "k" && @second === "f"
-      @output = (@input-273.15)*(9/5)+32
+      @output = (@input - 273.15) * 9/5 + 32
     elsif @first === "k" && @second === "c"
       @output = @input - 273.15
     else
